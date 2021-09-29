6f300f6de670d978ccffc571e085914c5ad4bdc1
Rasmus Sørensen <xxr@live.dk>
2021-09-29 18:07:39 +0200

    Cleanup old database system

--- cron.sh
+++ cron.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#rm /var/www/planningpoker.rasmusbundsgaard.dk/db.sqlite
-# How to 'clear'/reset database?
 php /var/www/planningpoker.rasmusbundsgaard.dk/bootstrap/migrations.php
 supervisorctl restart planningpoker:*
 systemctl restart nginx
