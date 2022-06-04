f387e2030ef06acffa174161cabdd4a9d6b1b75c
Rasmus Sørensen <xxr@live.dk>
2022-06-04 10:25:02 +0200

    Add seeder user using env to provide more anonymity

--- config/app.php
+++ config/app.php
@@ -5,4 +5,7 @@ return [
     'env' => env('APP_ENV', 'production'),
     'websocket_port' => env('WEBSOCKET_PORT', 9000),
     'debug' => (bool) env('APP_DEBUG', false),
+    'players' => explode(',', env('GAME_PLAYERS', '')),
+    'spectators' => explode(',', env('GAME_SPECTATORS', '')),
+    'game_masters' => explode(',',  env('GAME_GAME_MASTERS', '')),
 ];
